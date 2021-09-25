a07d186ef159904de1965a7f48c89bb3a525aed0
ExaInsanity <irisfeanora@outlook.com>
2021-09-25 12:03:01 +0200

    bot boilerplate

---
+++ EagleThreadBot/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace EagleThreadBot
+{
+	internal class Config
+	{
+		[JsonProperty("token")]
+		public String Token { get; set; }
+
+		[JsonProperty("channels")]
+		public UInt64[] AllowedChannels { get; set; }
+	}
+}
---
+++ EagleThreadBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using DSharpPlus;
+
+using Newtonsoft.Json;
+
+namespace EagleThreadBot
+{
+	internal static class Program
+	{
+		public static Config Configuration { get; private set; }
+		public static DiscordClient Client { get; private set; }
+
+		internal static async Task Main(String[] args)
+		{
+			StreamReader reader = new("./config.json");
+			Configuration = JsonConvert.DeserializeObject<Config>(reader.ReadToEnd());
+			reader.Close();
+
+			Client = new(new()
+			{
+				Token = Configuration.Token,
+				TokenType = TokenType.Bot
+			});
+
+			await Client.ConnectAsync();
+		}
+	}
+}
